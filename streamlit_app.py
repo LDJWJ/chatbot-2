f28408e971d5366dd8e409e3d3c6bc721ce14961
LDJWJ <frontierall@naver.com>
2024-11-28 14:29:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,31 @@
-# Streamlit 라이브러리 가져오기
-import streamlit as st
+import streamlit as st  
+from PIL import Image  
 
-# 애플리케이션 제목
-st.title("간단한 Streamlit 웹 애플리케이션")
+# 앱 제목  
+st.title("이미지 반으로 줄이기")  
 
-# 사용자 입력 받기
-user_input = st.text_input("메시지를 입력하세요:")
+# 이미지 업로드  
+uploaded_file = st.file_uploader("이미지를 업로드하세요", type=["jpg", "jpeg", "png"])  
 
-# 입력된 메시지 출력하기
-if user_input:
-    st.write(f"입력하신 메시지: {user_input}")
+if uploaded_file is not None:  
+    # 이미지 열기  
+    image = Image.open(uploaded_file)  
+    
+    # 원본 이미지 표시  
+    st.image(image, caption="원본 이미지", use_column_width=True)  
+    
+    # 이미지 크기 반으로 줄이기  
+    new_width = image.width // 2  
+    new_height = image.height // 2  
+    resized_image = image.resize((new_width, new_height))  
+    
+    # 줄인 이미지 표시  
+    st.image(resized_image, caption="줄인 이미지", use_column_width=True)  
+
+    # 줄인 이미지 다운로드  
+    st.download_button(  
+        label="줄인 이미지 다운로드",  
+        data=resized_image,  
+        file_name="resized_image.png",  
+        mime="image/png"  
+    )
